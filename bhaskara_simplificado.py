5bf2bef2334fdb3be3318af4b1594f646a17dced
asaphgomes <asaph.gomes@hotmail.com>
2023-02-22 22:38:58 -0300

    adicionei novas linhas nas respostas

--- bhaskara_simplificado.py
+++ bhaskara_simplificado.py
@@ -6,14 +6,14 @@ c = float(input("Qual o valor de c? "))
 delta = (b ** 2) - (4 * a * c)
 
 if delta < 0:
-    print("esta equação não possui raízes reais")
+    print("\nesta equação não possui raízes reais")
 elif delta == 0:
     x = (- b + math.sqrt(delta)) / (2 * a)
-    print(f"a raíz dupla desta equação é {x}")
+    print(f"\na raíz dupla desta equação é {x}")
 else:
     x1 = (- b + math.sqrt(delta)) / (2 * a)
     x2 = (- b - math.sqrt(delta)) / (2 * a)
     if x1 > x2:
-        print(f"As raízes da equação são {x2} e {x1}")
+        print(f"\nAs raízes da equação são {x2} e {x1}")
     else:
-        print(f"As raízes da equação são {x1} e {x2}")
+        print(f"\nAs raízes da equação são {x1} e {x2}")
